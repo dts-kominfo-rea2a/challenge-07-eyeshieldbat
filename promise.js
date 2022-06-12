f8d8c2106c41ff786f6533555ba77e531f5bc4d1
2nhard <eyeshield.bat@gmail.com>
2022-06-12 23:34:23 +0700

    Challenge 7 - Renhard Sitorus

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+  try {
+    const emosiIXX = await promiseTheaterIXX();
+    const emosiVGC = await promiseTheaterVGC();
+    const data = emosiIXX.concat(emosiVGC);
+
+    let total= 0
+    data.map(item => item.hasil === emosi && (total += 1))
+    return total;
+  } catch (err) {
+    return err
+  }
+    
+};
 
 module.exports = {
   promiseOutput,
